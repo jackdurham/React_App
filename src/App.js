05e06f725f9a8b54ac6d799e83a509568e9319ee
Jack Durham <jackdurham11@gmail.com>
2019-07-15 11:01:17 +0100

    new files created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
   render() {
@@ -7,6 +8,11 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <h1>Hi React App, I'm Jack</h1>
+        <Person name="Jack" age="25">
+          My Hobbies: Sleeping
+        </Person>
+        <Person name="Rane" age="25" />
+        <Person name="Will" age="27" />
       </div>
     );
   }
